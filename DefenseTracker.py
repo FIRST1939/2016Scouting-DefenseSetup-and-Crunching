be49ac84525d6127367af70836d06178f9c3e88f
vhcook <victoria.cook@gmail.com>
2016-02-21 15:34:07 -0600

    Set savefile and write buttons exist
    Got two buttons added - one to set the output file, and one to append
    the current match to the file.  The current match value is dummied out
    for the moment.

--- DefenseTracker.py
+++ DefenseTracker.py
@@ -10,6 +10,19 @@ game Stronghold.
 #!/usr/bin/env python 
 
 import tkinter as tk
+from tkinter import filedialog
+
+def saveCurrentMatch(matchData, filename):
+    '''(list, str)-> None
+    Takes the list containing the current matchdata and appends it to filename.
+    '''
+    
+    file=open(filename, mode='a')
+    outstr = str(matchData)+'\n'
+    file.write(outstr)
+    file.close()
+
+#from tkinter import *
 
 root = tk.Tk()
 app = tk.Frame(root)
@@ -20,17 +33,25 @@ top = app.winfo_toplevel()
 top.rowconfigure(1,weight=1)
 top.columnconfigure(1,weight=1)
 
-quitButton = tk.Button(text = 'Quit', command=tk._exit)
-quitButton.grid(column=1, row=1, sticky=tk.N+tk.S+tk.E+tk.W,padx=3)
+#COOK quit button if pushed, crashes python.  Don't push the button.
+#quitButton = tk.Button(text = 'Quit', command=tk._exit) 
+#quitButton.grid(column=1, row=1, sticky=N+S+E+W,padx=3)
 
 
+filename = tk.StringVar()
+filename.set('test')
 
 
+fileButton = tk.Button(root, text = 'Set savefile', 
+                       command=lambda: filename.set(filedialog.asksaveasfilename(title='Save filename')))
+fileButton.grid(column=1, row=1, sticky=tk.N+tk.S, padx=7)
+
+saveButton = tk.Button(root, text = 'Save data',
+                       command=lambda: saveCurrentMatch(['1','B1','A2','C1','D1','A1','C2','D2'],filename.get()))
+saveButton.grid(column=1, row=2, padx=7)
 
 
-
-
-
+#print(filename.get())
 
 
 
